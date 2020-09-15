101fbfd7805c3c943c8158236a874cf21b4bee3c
Vova Perebykivskyi <47381826+vova-pereb@users.noreply.github.com>
2020-09-15 13:00:02 +0300

    Update jenkins-prod.groovy

--- jenkins-prod.groovy
+++ jenkins-prod.groovy
@@ -1,2 +1,44 @@
 node {
+    
+    def gradleHome = tool 'GRADLE-6'
+
+        stage('Preparation') {
+        checkout([
+                $class: 'GitSCM',
+                branches: [[name: 'artifactory-jenkins']],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [],
+                submoduleCfg: [],
+                userRemoteConfigs: [[
+                        credentialsId: 'vova-github',
+                        url: 'https://github.com/cellrebel/CellRebelAndroid.git'
+                ]]
+        ])
+    }
+    
+        stage('Publish App to Artifactory') {
+            
+        rtGradleResolver(
+            id: 'cellrebel-gradle-resolver',
+            serverId: 'cellrebel-artifactory-prod'
+        )
+        
+        rtGradleDeployer(
+            id: 'cellrebel-gradle-deployer',
+            serverId: 'cellrebel-artifactory-prod',
+        )
+        
+        rtGradleRun(
+            tool: 'GRADLE-6',
+            usesPlugin: true,
+            useWrapper: false,
+            rootDir: 'sdk/',
+            buildFile: 'build.gradle',
+            resolverId: 'cellrebel-gradle-resolver',
+            deployerId: 'cellrebel-gradle-deployer',
+            tasks: 'clean assemble artifactoryPublish'
+        )
+        
+    }
+    
 }
